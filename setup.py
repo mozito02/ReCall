6bea65872d001b4ff9fbbf0be40d640a528730fe
ABIR CHAKRABORTY <107206976+mozito02@users.noreply.github.com>
2024-09-18 22:30:21 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
 from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    with open (file_path) as f:
+        requirements=f.readlines()
+        requirements=[r.replace('\n',"") for r in requirements]
+        
+        
+setup(
+   name='ReCall',
+   version='0.0.1',
+   description='Ann Churn Prediction model',
+   author='Abir Chakraborty',
+   author_email='abirc504@gmail.com',
+   packages=find_packages(),  #same as name
+   install_requires=get_requirements('requirements.txt')
+)
+
